f4b82e721ac4da2728372ede97ead99170fbb35e
Sub-esh <subsonicu571@gmail.com>
2025-06-06 21:40:04 +0545

    removed indentation bug in get_data.py

--- utils/get_data.py
+++ utils/get_data.py
@@ -2,5 +2,5 @@ import ee
 
 def get_Nepal_adm():
     # Load the admin boundaries of Nepal
-    Nepal_admin = ee.FeatureCollection"FAO/GAUL_SIMPLIFIED_500m/2015/level2")
+    Nepal_admin = ee.FeatureCollection("FAO/GAUL_SIMPLIFIED_500m/2015/level2")\
                     .filer(ee.filter.eq('ADM2_NAME','Bagmati'))
