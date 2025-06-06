ca2af4663438dfd7f63bf91d35990adcb2d086d2
Sub-esh <subsonicu571@gmail.com>
2025-06-06 21:25:16 +0545

    renamed component file and inserted script to fetch the data from GEE

---
+++ utils/get_data.py
@@ -0,0 +1,6 @@
+import ee
+
+def get_Nepal_adm():
+    # Load the admin boundaries of Nepal
+    Nepal_admin = ee.FeatureCollection"FAO/GAUL_SIMPLIFIED_500m/2015/level2")\
+                    .filer(ee.filter.eq('ADM2_NAME','Bagmati'))
