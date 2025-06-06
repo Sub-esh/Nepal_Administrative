6f2c87e79ba8b0eece2c23bc3da41ffebc9d9e83
Sub-esh <subsonicu571@gmail.com>
2025-06-06 21:47:10 +0545

    Created the main application logic in the main streamlit file.

--- Nepal_adminitrationApp.py
+++ Nepal_adminitrationApp.py
@@ -1,3 +1,20 @@
-import ee
-import streamlit
+import streamlit as st
+from utils.gee_auth import initialize_ee
+from utils.get_data import get_Nepal_adm
+from components.map_display import create_map
+
+# page config
+st.set_page_config(page_title = "Nepal Administrative Map", layout = "wide" )
+
+# title
+st.title ("Administrative Boundary of Nepal using Google Earth Engine")
+
+#Initialize GEE
+initialize_ee()
+
+# get data from gee
+Nepal_admin = get_Nepal_adm()
+
+# Create and display map
+m = create_map(Nepal_admin)
 
