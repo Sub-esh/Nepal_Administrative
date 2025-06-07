3255d76b30152aa28c763ba60f0d929fc9a0c507
Sub-esh <subsonicu571@gmail.com>
2025-06-07 08:20:49 +0545

    changed administrative boundaries to display all districts in get_data.py and added missing folium output function in Nepal_adminitrationApp.py

--- utils/get_data.py
+++ utils/get_data.py
@@ -3,5 +3,5 @@ import ee
 def get_Nepal_adm():
     # Load the admin boundaries of Nepal
     Nepal_admin = ee.FeatureCollection("FAO/GAUL_SIMPLIFIED_500m/2015/level2")\
-                    .filter(ee.Filter.eq('ADM2_NAME','Bagmati'))
+                    .filter(ee.Filter.inList('ADM0_NAME',['Nepal']))
     return Nepal_admin
