9d254a23d5a58608bad8368271dd25cf110f12bf
Sub-esh <subsonicu571@gmail.com>
2025-06-07 13:38:12 +0545

    Modified main app to reflect geemap functions

--- Nepal_adminitrationApp.py
+++ Nepal_adminitrationApp.py
@@ -15,9 +15,19 @@ initialize_ee()
 # get data from gee
 Nepal_admin = get_Nepal_adm()
 
-# Create and display map
-m = create_map(Nepal_admin)
+# extract district names for filtering
+features = Nepal_admin.getInfo()['features']
+district_names = sorted(set([f['properties']['ADM2_NAME'] for f in features]))
 
+# sidebar for filtering
+selected_district = st.sidebar.selectbox("Filter by Disctrict Name",['ALL']) + district_names
 
+# show full or filtered map
+if selected_district == "ALL":
+    st.subheader("All Districts")
+else:
+     st.subheader(f"Selected District: {selected_district}")
+
+create_map(Nepal_admin, selected_district if selected_district != "ALL" else None)
 
 
