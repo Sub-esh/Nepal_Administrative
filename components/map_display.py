267617224a725a91753f8b7c3f1be13786022591
Sub-esh <subsonicu571@gmail.com>
2025-06-07 13:26:18 +0545

    Removed folium library function with geemap functions

--- components/map_display.py
+++ components/map_display.py
@@ -1,22 +1,23 @@
-import folium
-from streamlit_folium import st_folium
+import geemap.foliumap as geemap
 
-def create_map(Nepal_admin):
-    # Convert GEE FeatureCollection to GeoJSON
-    geoj = Nepal_admin.getInfo()
+def create_map(Nepal_admin, selected_district = None):
+    # Create a geemap map object
+    m = geemap.Map(center=(28.3949,84.124),zoom = 9)
 
-    # Create Folium map centered on the Kathmandu
-    m = folium.Map(location = [28.3949,84.124], zoom_start = 7)
+    # filter by district
+    if selected_district:
+        district = Nepal_admin.filter(ee.Filter.eq("ADM2_NAME", selected_district))
+    else:
+        district = Nepal_admin
+    
+    # Add the the layer to the map
+    m.addLayer(district, {}, "Nepal Districts")
 
-    # Add the administrative boundaries
-    folium.GeoJson(
-        geoj,
-        name = "Nepal Adminstrative",
-        style_function = lambda feature: {
-            'fillColor' : '#4466cc',
-            'color' : 'black',
-            'weight': 1,
-            'fillOpacity' : 0.4
-        }
-    ).add_to(m)
-    return st_folium(m, width = 700, height=500)
+    # Set map to bounds of the districts
+    m.centerObject(Nepal_admin, zoom = 9)
+
+    # Add layer control
+    m.add_control(geemap.LayersControl())
+
+    # Display the map in Streamlit
+    return m.to_streamlit(width=1200, height=600)
