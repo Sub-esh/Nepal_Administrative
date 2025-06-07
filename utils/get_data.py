0afd218c31aa95c20440305cc1487f7dd57f490e
Sub-esh <subsonicu571@gmail.com>
2025-06-07 08:05:23 +0545

    added missing return statement in get_nepal_adm() function under get_data.py

--- utils/get_data.py
+++ utils/get_data.py
@@ -4,3 +4,4 @@ def get_Nepal_adm():
     # Load the admin boundaries of Nepal
     Nepal_admin = ee.FeatureCollection("FAO/GAUL_SIMPLIFIED_500m/2015/level2")\
                     .filter(ee.Filter.eq('ADM2_NAME','Bagmati'))
+    return Nepal_admin
